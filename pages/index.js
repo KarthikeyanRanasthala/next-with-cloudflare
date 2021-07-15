2ee34ab32c6c7b58d673c8fb1a5471458542c73b
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2021-07-15 22:22:36 +0530

    Add test images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import Image from 'next/image';
 
-const IndexPage = () => (<h1>Hello</h1>);
+import imgSrc from '../public/cristina-gottardi-CSpjU6hYo_0-unsplash.jpg';
+
+const IndexPage = () => (
+    <>
+        {/* This will fetch the image at full width and 75% of quality */}
+        <div style={{ width: '300px' }}>
+            <Image
+                src={imgSrc}
+            />
+        </div>
+        {/* This will fetch the image at closest preset width to 300px and 75% of quality */}
+        <Image
+            src={imgSrc}
+            width={300}
+            height={200}
+        />
+        {/* This will also do the same as above */}
+        <Image
+            src="/cristina-gottardi-CSpjU6hYo_0-unsplash.jpg"
+            width={300}
+            height={200}
+        />
+        {/* This will fetch the image according to the viewport width using srcset */}
+        <Image
+            src={imgSrc}
+            layout="responsive"
+        />
+    </>
+);
 
 export default IndexPage;
